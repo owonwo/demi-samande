a3100127067bd1d4bee3ac83abcdf54b0ee0fa55
Joseph Owonvwon <joseph.owonwo@gmail.com>
2024-11-22 04:57:14 +0100

    updated launch

--- src/libs/constants.ts
+++ src/libs/constants.ts
@@ -1,4 +1,4 @@
 export const BOOK_URL =
   "https://www.amazon.com/Her-Hands-Shaping-Future-Manufacturing-ebook/dp/B0D86Z3R8J?dplnkId=6e6ff0d2-b28e-4775-a732-72d8b82671f0";
 
-export const PreorderEndDate = new Date(2024, 11, 2);
+export const PreorderEndDate = new Date(2024, 11, 10);
